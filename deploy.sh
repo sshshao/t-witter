a89aa8cee34629e8088d21053e22a7db39a163bc
richackard <huangyueyang96@gmail.com>
2018-04-05 22:58:21 -0400

    Twisted a bit.

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,15 @@
 echo "Deployment Process Started."
 
 echo '\033[35m[x] Stopping Services... \033[0m'
-docker stack rm twitter
+sudo docker stack rm twitter
 
 echo '\033[35m[x] Updating Codebase... \033[0m'
 git pull origin master
 
 echo '\033[36m[x] Build Docker Images... \033[0m'
-sh build.sh
+sudo sh build.sh
 
 echo '\033[36m[x] Deploy Services... \033[0m'
-docker stack deploy -c docker-compose.yml twitter
+sudo docker stack deploy -c docker-compose.yml twitter
 
 echo '\033[36m[x] Deployment Finished... \033[0m'
