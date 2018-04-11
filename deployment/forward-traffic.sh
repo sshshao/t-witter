cb986bf3237f10a77d4f282fea2b72ed14bcb1b1
richackard <huangyueyang96@gmail.com>
2018-04-10 22:01:20 -0400

    final tweaks....

---
+++ deployment/forward-traffic.sh
@@ -0,0 +1,2 @@
+iptables -A PREROUTING -t nat -p tcp -i ens3 --dport 80 -j DNAT --to-destination 10.0.1.15:30001
+iptables -A FORWARD -p tcp -d 10.0.1.15 --dport 30001 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
