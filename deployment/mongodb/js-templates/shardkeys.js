c82dbee32d7ee51caaa2cc6d7710876fb450a700
richackard <huangyueyang96@gmail.com>
2018-04-13 18:58:15 -0400

    SHARD KEY

--- deployment/mongodb/js-templates/shardkeys.js
+++ deployment/mongodb/js-templates/shardkeys.js
@@ -1,2 +1,4 @@
-sh.enableSharding( "styxmail" )
-sh.shardCollection( "styxmail.styxlog", { "area" : 1, "_id" : 1 } )
+sh.enableSharding( "witter" )
+sh.shardCollection("witter.tweet", {"id": "hashed"})
+sh.shardCollection("witter.profile", {"id": "hashed"})
+//sh.shardCollection( "styxmail.styxlog", { "area" : 1, "_id" : 1 } )
