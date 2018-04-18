aa515a00f6184630a1a2fbf0dc9ee0de42c1e948
richackard <huangyueyang96@gmail.com>
2018-04-17 23:02:41 -0400

    Wo zhen de niu bi

--- gateway/app/direct_dispatcher.py
+++ gateway/app/direct_dispatcher.py
@@ -1,6 +1,6 @@
 import pika, uuid
 import configparser
-import os
+import os, sys
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -9,17 +9,30 @@ AMQP_HOST = config['AMQP']['AMQP_Host']
 AMQP_Exchange = config['AMQP']['AMQP_Exchange']
 AMQP_Exchange_Type = config['AMQP']['AMQP_Exchange_Type']
 
+def get_cur_time_milli():
+    t_ms = int(time.time() * 1000)
+    return t_ms
+
 class RPCDispatcher(object):
     def __init__(self, ):
+        bf_time = get_cur_time_milli()
         self.connection = pika.BlockingConnection(
             pika.ConnectionParameters(host=AMQP_HOST))
+        af_time = get_cur_time_milli()
+        sys.stderr.write("RPC Connection Creation Takes: %d ms\n" % (af_time - bf_time))
 
+        bf_time = get_cur_time_milli()
         self.channel = self.connection.channel()
+        af_time = get_cur_time_milli()
+        sys.stderr.write("RPC Channel Creation Takes: %d ms\n" % (af_time - bf_time))
 
+        bf_time = get_cur_time_milli()
         self.channel.basic_consume(
             self.on_response,
             queue='amq.rabbitmq.reply-to',
             no_ack=True)
+        af_time = get_cur_time_milli()
+        sys.stderr.write("RPC Callback and Queue Binding Takes: %d ms\n" % (af_time - bf_time))
     
     def on_response(self, ch, method, props, body):
         self.response = body
