2801e5d663bfb9cd882f5d6fc21463d09f74d66f
richackard <huangyueyang96@gmail.com>
2018-04-21 17:54:46 -0400

    Added test api script.

---
+++ api-gateway.sh
@@ -0,0 +1,21 @@
+
+cd deployment
+kubectl delete -f api-gateway-deployment.yml
+
+git checkout m3-node
+git pull origin m3-node
+
+cd ../gateway
+echo '\033[34m[x] Building API Gateway Service...\033[0m'
+docker image build -q -t twitter_gateway_service .
+
+# Tagging and Pushing Gateway Service.
+echo '\033[35m[x] Tagging Gateway Service...\033[0m'
+docker tag twitter_gateway_service richackard/twitter_gateway_service:latest
+
+echo '\033[35m[x] Pushing Gateway Service...\033[0m'
+docker push richackard/twitter_gateway_service:latest
+
+cd ../deployment
+
+kubectl apply -f api-gateway-deployment.yml
