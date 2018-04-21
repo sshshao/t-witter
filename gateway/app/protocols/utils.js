0d3ea3accf8aa5a2e5df756ab3bcda6ee7443efa
S Shao <sshao14@gmail.com>
2018-04-21 16:09:18 -0400

    fixed typo

--- gateway/app/protocols/utils.js
+++ gateway/app/protocols/utils.js
@@ -1,7 +1,7 @@
 const STATUS_OK = 'OK';
 const STATUS_ERROR = 'error';
 
-exports.generate_message = function(status, msg) {
+exports.generateMessage = function(status, msg) {
     if (status == STATUS_OK) {
         return {
             'status': STATUS_OK,
