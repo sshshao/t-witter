e8ab77f0b62ee6bd5f7da7649964c70883d2a9f4
richackard <huangyueyang96@gmail.com>
2018-04-24 22:49:36 -0400

    Node Js tho

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,7 @@ kubectl delete -f api-gateway-deployment.yml
 kubectl delete -f auth-service-deployment.yml
 kubectl delete -f email-service-deployment.yml
 kubectl delete -f profile-service-deployment.yml
+kubectl delete -f memcached-service-deployment.yml
 kubectl delete -f tweet-service-deployment.yml
 
 cd ..
@@ -23,5 +24,7 @@ kubectl apply -f auth-service-deployment.yml
 kubectl apply -f email-service-deployment.yml
 kubectl apply -f profile-service-deployment.yml
 kubectl apply -f tweet-service-deployment.yml
+kubectl apply -f memcached-service-deployment.yml
+
 
 echo '\033[36m[x] Deployment Finished... \033[0m'
