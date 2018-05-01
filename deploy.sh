3d454f9cc9b2f70ef13a27e58d7ce9b6feb96841
richackard <huangyueyang96@gmail.com>
2018-04-30 23:14:15 -0400

    Deployment scripts will automatically clean the database.

--- deploy.sh
+++ deploy.sh
@@ -19,6 +19,9 @@ sudo sh build.sh
 
 echo '\033[35m[x] Deploying new Services... \033[0m'
 cd deployment
+sleep 5
+echo '\033[35m[x] Clearing Databases.. \033[0m'
+./utils/clear.sh
 kubectl apply -f api-gateway-deployment.yml
 kubectl apply -f auth-service-deployment.yml
 kubectl apply -f email-service-deployment.yml
