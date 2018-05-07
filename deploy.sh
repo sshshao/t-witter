03cb63b0bbc57bf49393a94d92fcc1f28aab4bb8
S Shao <sshao14@gmail.com>
2018-05-07 18:04:56 -0400

    testing text query again

--- deploy.sh
+++ deploy.sh
@@ -13,8 +13,8 @@ kubectl delete -f tweet-service-deployment.yml
 
 cd ..
 echo '\033[35m[x] Updating Codebase... \033[0m'
-git checkout m4
-git pull origin m4
+git checkout m4-mg
+git pull origin m4-mg
 sudo sh build.sh
 
 echo '\033[35m[x] Deploying new Services... \033[0m'
